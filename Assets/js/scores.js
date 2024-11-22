6c06d1ce797ca92ca3698c228e5705c536033f02
Unk171 <7353687@gmail.com>
2024-11-21 23:04:09 -0500

    start

--- Assets/js/scores.js
+++ Assets/js/scores.js
@@ -1,6 +1,6 @@
 function printHighscores() {
   // either get scores from localstorage or set to empty array
-  var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+  const highscores = JSON.parse(localStorage.getItem('highscores')) || [];
 
   // sort highscores by score property in descending order
   highscores.sort(function (a, b) {
@@ -19,8 +19,8 @@ function printHighscores() {
 }
 
 function clearHighscores() {
-  window.localStorage.removeItem('highscores');
-  window.location.reload();
+  localStorage.removeItem('highscores');
+  location.reload();
 }
 
 document.getElementById('clear').onclick = clearHighscores;
