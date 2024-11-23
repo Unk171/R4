5fdc2b36349c1c145cb06f558fcd8bc079a55d3d
Unk171 <7353687@gmail.com>
2024-11-23 00:31:01 -0500

    made js frame

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,16 +1,39 @@
+const start = document.querySelector("#start");
 const question = document.querySelector("#question");
 const answerOptions = document.querySelector("#answerOptions");
+const quizWindow = document.querySelector("#quizWindow");
+const startWindow = document.querySelector("#startWindow");
+const result = document.querySelector("#result");
 
-const quetsionIndex = Math.floor(Math.random()*questions.length);
+function quiz() {
 
-question.textContent = questions[quetsionIndex].question;
+    answerOptions.innerHTML = "";
+    const quetsionIndex = Math.floor(Math.random() * questions.length);
+    const currentQuestion = questions[quetsionIndex]
+    question.textContent = currentQuestion.question;
+    currentQuestion.options.forEach((option, i) => {
+        const li = document.createElement("li");
+        li.setAttribute("data-id", i);
+        li.textContent = option;
+        li.addEventListener("click", function () {
+            const answerIndexString = li.getAttribute("data-id");
+            const answerIndex = Number(answerIndexString);
+            if (answerIndex === questions[quetsionIndex].correctAnswer) {
+                result.textContent = "Correct";
+                quiz()
+            } else {
+                result.textContent = "Wrong";
+                quiz()
+            }
+        })
+        answerOptions.appendChild(li)
+    });
 
+}
 
-for (let i= 0; i < questions[quetsionIndex].options.length; i++) {
-    const li = document.createElement("li");
-    li.id = `option ${i}`
-    li.textContent = questions[quetsionIndex].options[i];
-    answerOptions.appendChild(li)
-} 
-
+start.addEventListener("click", function () {
+    quizWindow.style.display = "block";
+    startWindow.style.display = "none";
+    quiz()
+})
 
